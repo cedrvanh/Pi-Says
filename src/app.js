9a178794a14cf50c0ce5eb6fa27d5a44b6b20b06
cedrvanh <cedrvanh@student.arteveldehs.be>
2020-01-17 01:14:46 +0100

    Started working on user flow + added audio files

--- src/app.js
+++ src/app.js
@@ -2,8 +2,16 @@ import { initFirebase } from './js/config';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './styles/main.scss';
 
+let buttons = document.querySelectorAll('.board__section');
+let playControl = document.querySelector('.board__controls-start');
+let scoreControl = document.querySelector('.board__controls-score');
+
+let sequence = [];
+let user = [];
+let score = 0;
+let color;
 let hasStarted = false;
-let audioFiles = [
+const audioFiles = [
     'http://www.soundjay.com/button/sounds/button-4.mp3',
     'http://www.soundjay.com/button/sounds/button-5.mp3',
     'http://www.soundjay.com/button/sounds/button-6.mp3',
@@ -16,9 +24,13 @@ const getRandomNumber = () => {
 };
 
 const startGame = () => {
+    // Initialize button classes
+    initButtonClasses();
+    // Initialize button click events 
     initButtonListeners();
-    console.log('Clicked');
-    hasStarted = true
+    // Initialize Simon Says sequence
+    initSequence();
+    hasStarted = true;
 };
 
 const playAudio = (id) => {
@@ -27,19 +39,89 @@ const playAudio = (id) => {
     audio.play();
 };
 
-const initButtonListeners = () => {
-    let buttons = document.querySelectorAll('.board__section');
+const initButtonClasses = () => {
+    toggleClass(playControl, 'is-hidden');
+    toggleClass(scoreControl, 'is-hidden');
+}
 
+const initButtonListeners = () => {
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             const color = button.className.split(" ")[1];
-            const id = button.id;
-            console.log(`Clicked button ${color}`)
-
-            playAudio(id)
+            const id = JSON.parse(button.id);
+            handleUserInput(id);
+            // nextSequence();
         });
     });
 };
 
-let playBtn = document.querySelector('#playBtn');
-playBtn.addEventListener('click', startGame);
+const initSequence = () => {
+    const currentSequence = getRandomNumber();
+    sequence.push(currentSequence);
+    addButtonEvent(currentSequence, getColorById(currentSequence));
+}
+
+const nextSequence = () => {
+    const nextSequence = getRandomNumber();
+    sequence.push(nextSequence);
+    addButtonEvent(nextSequence, getColorById(nextSequence));
+}
+
+const validateSequence = () => {
+    for(let i = 0; i < user.length; i++) {
+        if(user[i] !== sequence[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const handleUserInput = (id) => {
+    const color = getColorById(id);
+    console.log(`Clicked button ${color}`);
+    user.push(id);
+    console.log(validateSequence());
+}
+
+const getColorById = (id) => {
+    let color;
+    switch (id) {
+        case 0:
+            color = 'green';
+            break;
+        case 1:
+            color = 'red';
+            break;
+        case 2:
+            color = 'yellow';
+            break;
+        case 3:
+            color = 'blue';
+            break;
+        default:
+            break;
+    }
+    return color;
+}
+
+const changeScore = () => {
+    score++;
+    scoreControl.innerHTML += score;
+}
+
+const addButtonEvent = (id, color = 'green') => {
+    toggleButton(id, color);
+    // playAudio(id);    
+}
+
+const toggleButton = (id, color) => {
+    const button = document.getElementById(`${id}`);
+    toggleClass(button, `${color}--active`);
+    setTimeout(() => toggleClass(button, `${color}--active`), 1000);
+}
+
+const toggleClass = (element, className) => {
+    element.classList.toggle(`${className}`);
+};
+
+playControl.addEventListener('click', startGame);
